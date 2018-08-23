b29e96b003afe41538b9ad02584ec7e8b80fe09b
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-08-23 13:30:04 +0300

    Add inspection "Redundant runCatching {}" (related to KT-25621)

---
+++ idea/testData/inspectionsLocal/coroutines/redundantRunCatching/simple.kt
@@ -0,0 +1,9 @@
+package kotlin
+
+class SuccessOrFailure<T>(val value: T?) {
+    fun getOrThrow(): T = value ?: throw AssertionError("")
+}
+
+fun <T> runCatching(block: () -> T) = SuccessOrFailure(block())
+
+fun correct(arg: Boolean) = runCatching<caret> { if (arg) throw AssertionError("") else 12 }.getOrThrow()
