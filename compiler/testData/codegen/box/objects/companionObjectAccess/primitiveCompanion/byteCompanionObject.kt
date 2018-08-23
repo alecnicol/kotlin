38ba88449ebd311a1e2e09154290b3d92193da73
Roman Artemev <roman.artemev@jetbrains.com>
2018-08-23 17:23:36 +0300

    Add tests for primitive companion object

---
+++ compiler/testData/codegen/box/objects/companionObjectAccess/primitiveCompanion/byteCompanionObject.kt
@@ -0,0 +1,18 @@
+
+fun Byte.Companion.MAX() = MAX_VALUE
+fun Byte.Companion.MIN() = MIN_VALUE
+
+fun <T> test(o: T) { assertEquals(o === Byte.Companion, true) }
+
+fun box(): String {
+
+    assertEquals(127, Byte.MAX_VALUE)
+
+    assertEquals(Byte.MIN_VALUE, Byte.MIN())
+    assertEquals(Byte.MAX_VALUE, Byte.Companion.MAX())
+
+    test(Byte)
+    test(Byte.Companion)
+
+    return "OK"
+}
---
+++ compiler/testData/codegen/box/objects/companionObjectAccess/primitiveCompanion/intCompanionObject.kt
@@ -0,0 +1,19 @@
+
+
+
+fun Int.Companion.MAX() = MAX_VALUE
+fun Int.Companion.MIN() = MIN_VALUE
+
+fun <T> test(o: T) { assertEquals(o === Int.Companion, true) }
+
+fun box(): String {
+    assertEquals(2147483647, Int.MAX_VALUE)
+
+    assertEquals(Int.MIN_VALUE, Int.MIN())
+    assertEquals(Int.MAX_VALUE, Int.Companion.MAX())
+
+    test(Int)
+    test(Int.Companion)
+
+    return "OK"
+}
